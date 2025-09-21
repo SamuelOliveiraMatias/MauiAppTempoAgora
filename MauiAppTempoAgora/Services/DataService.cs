156e7c174b8e2c2da856e480763345fa6ecb8c9d
Samuel Oliveira Matias <samuel.matias01@etec.sp.gov.br>
2025-09-21 14:31:21 -0300

    ag7

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,63 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+	    try 
+	    {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+		if(resp.StatusCode == HttpStatusCode.NotFound)
+		{
+			await Shell.Current.DisplayAlert("Erro", "Cidade não encontrada. Verifique o nome digitado.", "OK");
+			return null;
+		}
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    }; // Fecha obj do Tempo.
+                } // Fecha if se o status do servidor foi de sucesso
+            } // fecha laço using
+	    } catch (HttpRequestException){
+		    await Shell.Current.DisplayAlert("Erro", "Sem conexão com a internet. Verifique sua internet e tente novamente.", "OK");
+		    return null;
+	    }
+
+            return t;
+        }
+    }
+}
